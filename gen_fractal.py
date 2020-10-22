4821d5ef51606725a6d343eb1744aa784ed59a8b
JStocke12 <56052177+JStocke12@users.noreply.github.com>
2020-10-22 15:31:25 -0400

    Added tricolor functionality

--- gen_fractal.py
+++ gen_fractal.py
@@ -7,7 +7,7 @@ height = 1024
 coordinateWindowSize = 2.0
 
 iterations = 10
-roots = [complex(1),complex(-0.5,1.5**0.5),complex(-0.5,-(1.5**0.5))]
+roots = [complex(1),complex(-0.5,0.5*3**0.5),complex(-0.5,-0.5*3**0.5)]
 epsilon = 0.1
 
 img = Image.new('RGB', (width, height))
@@ -21,9 +21,11 @@ for i in range(width):
         newtonMethodArr = [complex(x,y)]
         for k in range(iterations):
             newtonMethodArr.append(newtonMethodArr[-1]-(newtonMethodArr[-1]**3-1)/(3*newtonMethodArr[-1]**2) if newtonMethodArr[-1] != complex(0) else 0)
-        if any([epsilon>abs(newtonMethodArr[-1]-i) for i in roots]):
-            px[i,j] = (255,255,255)
+        px[i,j] = tuple(int(192/([epsilon>abs(k-i) for k in newtonMethodArr].index(True)+1))+63 if epsilon>abs(newtonMethodArr[-1]-i) else 0 for i in roots)
+        """if any([epsilon>abs(newtonMethodArr[-1]-i) for i in roots]):
+            shade = int(192/([epsilon>abs(i-roots[0]) for i in newtonMethodArr].index(True)+1))+63
+            px[i,j] = (shade,shade,shade)
         else:
-            px[i,j] = (0,0,0)
+            px[i,j] = (0,0,0)"""
 
 img.save('image.png')
