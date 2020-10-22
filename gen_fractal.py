087689726ca4b16cfa004673147b72c3bdd57056
JStocke12 <56052177+JStocke12@users.noreply.github.com>
2020-10-22 16:02:22 -0400

    Reduced for loop to list comprehension

--- gen_fractal.py
+++ gen_fractal.py
@@ -16,12 +16,7 @@ px = img.load()
 
 for i in range(width):
     for j in range(height):
-        x = (2*i/width-1)*coordinateWindowSize
-        y = (1-2*j/width)*coordinateWindowSize
-        newtonMethodArr = [z := complex(x,y)]
-        #newtonMethodArr += [z := ((z-z**3-1)/(3*z**2) if z != complex(0) else 0) for i in range(iterations)]
-        for k in range(iterations):
-            newtonMethodArr.append(newtonMethodArr[-1]-(newtonMethodArr[-1]**3-1)/(3*newtonMethodArr[-1]**2) if newtonMethodArr[-1] != complex(0) else 0)
+        newtonMethodArr = [z := complex((2*i/width-1)*coordinateWindowSize,(1-2*j/width)*coordinateWindowSize)] + [z := (z-(z**3-1)/(3*z**2) if z != complex(0) else 0) for i in range(iterations)]
         px[i,j] = tuple(int(192/([epsilon>abs(k-i) for k in newtonMethodArr].index(True)+1))+63 if epsilon>abs(newtonMethodArr[-1]-i) else 0 for i in roots)
 
 img.save('image.png')
