ac141a535eb0d7a2e7a51eb00c49b4702ee96859
JStocke12 <56052177+JStocke12@users.noreply.github.com>
2020-10-22 15:33:51 -0400

    Removed unecessary comment

--- gen_fractal.py
+++ gen_fractal.py
@@ -22,10 +22,5 @@ for i in range(width):
         for k in range(iterations):
             newtonMethodArr.append(newtonMethodArr[-1]-(newtonMethodArr[-1]**3-1)/(3*newtonMethodArr[-1]**2) if newtonMethodArr[-1] != complex(0) else 0)
         px[i,j] = tuple(int(192/([epsilon>abs(k-i) for k in newtonMethodArr].index(True)+1))+63 if epsilon>abs(newtonMethodArr[-1]-i) else 0 for i in roots)
-        """if any([epsilon>abs(newtonMethodArr[-1]-i) for i in roots]):
-            shade = int(192/([epsilon>abs(i-roots[0]) for i in newtonMethodArr].index(True)+1))+63
-            px[i,j] = (shade,shade,shade)
-        else:
-            px[i,j] = (0,0,0)"""
 
 img.save('image.png')
